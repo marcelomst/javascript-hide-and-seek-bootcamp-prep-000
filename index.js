c908cca3d3ebcb9c9da688cce3a3a45b7ae36e40
Marcelo Martinez <marcelomst1@gmail.com>
2020-01-04 20:05:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+function nestedTarget() {
+   return document.querySelector('#nested .target');
+}
+function increaseRankBy(n){
+  var ElementsRanquedList = document.querySelectorAll('ul.ranked-list li');
+  var newRank = 0;
+  for (let i = 0; i < ElementsRanquedList.length; i++) {
+    newRank =  parseInt(ElementsRanquedList[i].innerHTML) + n;
+    ElementsRanquedList[i].innerHTML = newRank.toString();
+  }
+}
+function deepestChild(){
+  const grandnodelist = document.querySelectorAll('div#grand-node div');
+  return grandnodelist[grandnodelist.length-1];
+}
